389efbfd90daed8afe6842c1df6324047e57280d
Jim Teeuwen <jimteeuwen@gmail.com>
2016-07-11 17:50:39 +0200

    Bumps the minor version number.
    Signed-off-by: Jim Teeuwen <jimteeuwen@gmail.com>

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ import (
 const (
 	AppName         = "autimaat"
 	AppVersionMajor = 0
-	AppVersionMinor = 15
+	AppVersionMinor = 16
 )
 
 // AppVersionRevision defines the application build number.

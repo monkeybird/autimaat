1571b9ade15b655713c3e572baf20c1a5e7f61e2
Jim Teeuwen <jimteeuwen@gmail.com>
2016-09-04 21:35:56 +0200

    Fixes `stats` plugin behaviour. It removes seperation of user lists per channel. This separation does not work when it comes to NICK requests. Those are server-wide and do not carry any channel information to filter on. We therefore now apply a single user list for the entire network. This means that the whois command can be run in a PM to the bot.
    Signed-off-by: Jim Teeuwen <jimteeuwen@gmail.com>

--- plugins/stats/strings.go
+++ plugins/stats/strings.go
@@ -7,9 +7,8 @@ const (
 	// ref: https://godoc.org/time#Time.Format
 	TextWhoisDateFormat = "2 January, 2006"
 
-	TextWhoisName         = "wieis"
-	TextWhoisNick         = "gebruiker"
-	TextWhoisDisplay      = "%s, ik ken %s sinds %s als: %s"
-	TextWhoisNotInChannel = "Het !wieis commando werkt alleen in een kanaal."
-	TextWhoisUnknownUser  = "%s, ik herken %s niet."
+	TextWhoisName        = "wieis"
+	TextWhoisNick        = "gebruiker"
+	TextWhoisDisplay     = "%s, ik ken %s sinds %s als: %s"
+	TextWhoisUnknownUser = "%s, ik herken %s niet."
 )

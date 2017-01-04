baaf0b5a375b852afd250ebbb3d8b17d3ba13035
mouzaf <mouzaf@users.noreply.github.com>
2017-01-04 20:00:02 +0100

    Remove Wait() and doFork() from the proc package.
    These functions have been copied to bot.go in the previous commit. We
    need them there to have the full Bot struct available as an argument.

--- app/proc/proc.go
+++ app/proc/proc.go
@@ -15,10 +15,7 @@ package proc
 
 import (
 	"flag"
-	"log"
 	"os"
-	"os/exec"
-	"os/signal"
 	"strconv"
 	"syscall"
 )
@@ -29,46 +26,6 @@ var connectionCount uint
 
 func init() {
 	flag.UintVar(&connectionCount, "fork", 0, "Number of inherited file descriptors")
-}
-
-// Wait polls for OS signals to either kill or fork this process.
-//
-// The signals it waits for are: SIGKILL, SIGINT, SIGTERM and SIGUSR1.
-// The latter one being responsible for forking this process. The others
-// are there so we may cleanly exit this process.
-func Wait(argv []string, clients ...*os.File) {
-	signals := make(chan os.Signal, 1)
-	signal.Notify(
-		signals,
-		syscall.SIGKILL,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGUSR1,
-	)
-
-	// If the bot is run for the first time in a new session,
-	// it should be forked at least once to play nice with systemd.
-	if connectionCount == 0 {
-		log.Println("[proc] forking process...")
-		err := doFork(argv, clients...)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-
-	log.Println("[proc] Waiting for signals...")
-	for sig := range signals {
-		log.Println("[proc] received signal:", sig)
-		if sig != syscall.SIGUSR1 {
-			return
-		}
-
-		log.Println("[proc] forking process...")
-		err := doFork(argv, clients...)
-		if err != nil {
-			log.Println(err)
-		}
-	}
 }
 
 // Kill sends SIGINT to the current process. This can be used to cleanly
@@ -83,34 +40,6 @@ func KillParent() { syscall.Kill(os.Getppid(), syscall.SIGINT) }
 // Fork sends SIGUSR1 to the current process. This kickstarts the
 // forking process.
 func Fork() { syscall.Kill(os.Getpid(), syscall.SIGUSR1) }
-
-// doFork forks the current process into a child process and passes the
-// given client connections along to be inherited.
-//
-// The specified argv list define custom command line parameters which should
-// be used in the invocation.
-//
-// The client list contains any file descriptors which should be inherited
-// by the client.
-//
-// The forked process is called with the `-fork N` command line parameter.
-// Where N is the number of file descriptors being passed along. This is
-// used by the InheritedFiles() call below to rebuild the files.
-func doFork(argv []string, clients ...*os.File) error {
-	// Build the command line arguments for our child process.
-	// This includes any custom arguments defined in the profile.
-	args := append([]string{"-fork", strconv.Itoa(len(clients))}, argv...)
-
-	// Initialize the command runner.
-	cmd := exec.Command(os.Args[0], args...)
-	cmd.ExtraFiles = make([]*os.File, len(clients))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.ExtraFiles = clients
-
-	// Fork the process.
-	return cmd.Start()
-}
 
 // InheritedFiles returns a list of N file descriptors inherited from a
 // previous session through the Fork call.

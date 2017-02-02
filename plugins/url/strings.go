a1e2882c993369129c588676988d5967da099987
mouzaf <mouzaf@users.noreply.github.com>
2017-02-02 20:17:30 +0100

    Some (3) more url titles to ignore: cookie messages.

--- plugins/url/strings.go
+++ plugins/url/strings.go
@@ -10,4 +10,7 @@ const (
 
 var Ignore = map[string]bool{
 	"Imgur: The most awesome images on the Internet": true,
+	"Cookies op AD.nl | AD.nl":                       true,
+	"Cookies op Trouw.nl":                            true,
+	"Cookies op gelderlander.nl | gelderlander.nl":   true,
 }

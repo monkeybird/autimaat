d2870aaa1af28e4ff8c9ddae18f297a17a64bcd4
mouzaf <mouzaf@users.noreply.github.com>
2017-02-05 12:41:20 +0100

    Added title to ignore and small comment for the Ignore map.

--- plugins/url/strings.go
+++ plugins/url/strings.go
@@ -8,9 +8,12 @@ const (
 	TextYoutubeDuration = " (speelduur: %s)"
 )
 
+// Ignore is a map of title strings to ignore. Only exact matches will
+// be ignored.
 var Ignore = map[string]bool{
 	"Imgur: The most awesome images on the Internet": true,
-	"Cookies op AD.nl | AD.nl":                       true,
-	"Cookies op Trouw.nl":                            true,
-	"Cookies op gelderlander.nl | gelderlander.nl":   true,
+	"Tweakers":                                     true,
+	"Cookies op AD.nl | AD.nl":                     true,
+	"Cookies op Trouw.nl":                          true,
+	"Cookies op gelderlander.nl | gelderlander.nl": true,
 }
